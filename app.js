47afb0afd16beb1a5104490670c5492486af69b1
Austin Blackman <austin.blackman@coheretechnology.com>
2017-01-06 14:19:39 -0500

    circles added when click on new area in window.  cirles expand when clicked again, contract when right clicked

--- app.js
+++ app.js
@@ -1,51 +1,79 @@
-var data = [
-  {name: "Locke",    value:  4},
-  {name: "Reyes",    value:  8},
-  {name: "Ford",     value: 15},
-  {name: "Jarrah",   value: 16},
-  {name: "Shephard", value: 23},
-  {name: "Kwon",     value: 42}
-];
+var nodeData = [{x: 30, y: 50, key: 0, size: 1},
+              {x: 150, y: 180, key: 1, size: 3},
+              {x: 165, y: 132, key: 2, size: 15},
+              {x: 121, y: 180, key: 3, size: 12},
+              {x: 133, y: 512, key: 4, size: 4},
+              {x: 190, y: 520, key: 5, size: 8}]
+
+var width, height, vis, selector;
+var index = 6;
 
 var writeChart = function(){
-  var width = 420,
-      barHeight = 20;
-
-  var x = d3.scaleLinear()
-      .range([0, width]);
-
-  var chart = d3.select(".chart")
-      .attr("width", width);
-
-  x.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-  chart.attr("height", barHeight * data.length);
-
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-  bar.append("rect")
-      .attr("width", function(d) { return x(d.value); })
-      .attr("height", barHeight - 1);
-
-  bar.append("text")
-        .attr("x", function(d) { return x(d.value) - 15; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d.value; });
+  vis.selectAll('circle .node').data(nodeData).enter()
+    .append('svg:circle')
+    .attr('class', 'node')
+    .attr('cx', function(d){return d.x;})
+    .attr('cy', function(d){return d.y})
+    .attr('r', function(d){return d.size+'px'})
+    .attr('fill', 'rgb(138, 134, 216)')
+    .attr('data-key', function(d){return d.key})
 }
 
-var type = function(d){
-  d.value = +d.value; // coerce to number
-  return d;
+var initChart = function(){
+  height = window.innerHeight;
+  width = window.innerWidth;
+  vis = d3.select('#graph').append('svg');
+  vis.attr('width', width).attr('height',height);
+  vis.text('The Graph').select('#graph');
+  writeChart();
+}
+
+var addNewNode = function(evt){
+  nodeData.push({x:evt.clientX, y:evt.clientY, key: index, size: 5});
+  vis.append('svg:circle')
+  .attr('class', 'node')
+  .attr('cx', evt.clientX)
+  .attr('cy', evt.clientY)
+  .attr('r', function(d){return nodeData[nodeData.length-1].size+'px'})
+  .attr('fill', 'rgb(229, 144, 177)')
+  .attr('data-key', function(){index++; return nodeData[nodeData.length-1].key})
+}
+
+var removeNode = function(target){
+  console.log(target.dataset.key);
+  for(var i = 0;i<nodeData.length;i++){
+    if(nodeData[i].key == target.dataset.key){
+      nodeData.splice(i, 1);
+      selector = '[data-key="'+target.dataset.key+'"]';
+      d3.select(selector).data([]).exit().remove();
+    }
+  }
+}
+var modNodeSize = function(target, sign){
+  var oldVal = parseInt(vis.select('[data-key="'+target.dataset.key+'"]').attr('r'));
+  vis.select('[data-key="'+target.dataset.key+'"]').attr('r',function(){
+     return (oldVal+sign*oldVal/2) > 1 ? (oldVal+sign*oldVal/2) +'px' : '1px'
+   });
 }
 
 window.onload = function(){
-  document.querySelector('#d3Btn').addEventListener('click', function(){
-    console.log('here');
-    writeChart();
+  initChart();
+  document.addEventListener('click', function(evt){
+    if(evt.target.classList.contains('node')){
+      //removeNode(evt.target);
+      modNodeSize(evt.target, 1);
+    }
+    else{
+      addNewNode(evt);
+    }
   });
 
+  document.addEventListener('contextmenu', function(evt){
+    if(evt.target.classList.contains('node')){
+      evt.preventDefault();
+      //removeNode(evt.target);
+      modNodeSize(evt.target, -1);
+    }
+
+  })
 }
