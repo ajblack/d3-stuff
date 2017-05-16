5df60cf0348fda1062100785e2a5e34338910c05
Austin Blackman <austin.blackman@gmail.com>
2017-05-16 10:50:04 -0400

    drawing line between coordinates with standard path

--- mapapp.js
+++ mapapp.js
@@ -1,4 +1,52 @@
 window.onload = function(){
+  var data =
+  {
+    "fields":
+    [
+      {"name":"start_lat"},
+      {"name":"start_lon"},
+      {"name":"end_lat"},
+      {"name":"end_lon"}
+    ],
+    "rows":
+    [
+      ["-122.6795","45.5128","-46.62890","-23.54640"],
+      ["-122.6795","45.5128","151.19820","-33.86120"]
+    ]
+  }
+
+  var retCords = []
+  for(var i=0;i<data.rows.length*2;i++){
+    var ret = {"type":"Feature","geometry":{"type":"Point","coordinates":[0,0]},"properties":{"timestampMs":0}};
+    retCords.push(ret);
+  }
+
+  console.log(retCords);
+
+
+/*
+  for(var i=0;i<data.rows.length;i++){
+    var ret =
+    [
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[0,0]},"properties":{"timestampMs":0}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[0,0]},"properties":{"timestampMs":0}}
+    ]
+  }*/
+
+  //map the coordinates of each row value (set of two coordinates) individually to a retCords coordinate
+  var currentRet = 0;
+
+  for(var j=0;j<data.rows.length;j++){
+    retCords[currentRet].geometry.coordinates[0] = data.rows[j][0];
+    retCords[currentRet].geometry.coordinates[1] = data.rows[j][1];
+    currentRet++;
+    retCords[currentRet].geometry.coordinates[0] = data.rows[j][2];
+    retCords[currentRet].geometry.coordinates[1] = data.rows[j][3];
+    currentRet++;
+  }
+
+  var latlong = {"type":"FeatureCollection", "features":[]}
+  latlong.features = retCords;
 
 
   var width = 960,
@@ -10,35 +58,67 @@ window.onload = function(){
 
   var projection = d3.geoEquirectangular()
     .scale(153)
-      .translate([width/2,height/2])
+    .translate([width/2,height/2])
 
-
-  var path = d3.geoPath()
+  var pathGenerator = d3.geoPath()
       .projection(projection)
 
-//mexico city, bejing, pyongyang, honolulu
-  var latlong = {"type":"FeatureCollection","features":[
-    {"type":"Feature","geometry":{"type":"Point","coordinates":[-99.1332,19.4326]},"properties":{"timestampMs":1415894666875}},
-    {"type":"Feature","geometry":{"type":"Point","coordinates":[116.4,39.9]},"properties":{"timestampMs":1415894666875}},
-    {"type":"Feature","geometry":{"type":"Point","coordinates":[125.76,39.04]},"properties":{"timestampMs":1415894666875}},
-    {"type":"Feature","geometry":{"type":"Point","coordinates":[-157.8583,21.3069]},"properties":{"timestampMs":1415894666875}}
-  ]};
       svg.append("g")
         .attr("class", "land")
-      .selectAll("path")
+        .selectAll("path")
         .data([topojson.feature(world, world.objects.land)])
-        .enter().append("path")
-        .attr("d", path);
+        .enter()
+        .append("path")
+        .attr("d", pathGenerator);
+
     svg.append("g")
         .attr("class", "boundary")
         .selectAll("boundary")
         .data([topojson.feature(world, world.objects.countries)])
-        .enter().append("path")
-        .attr("d", path);
-    svg.append( "g" ).selectAll("path")
+        .enter()
+        .append("path")
+        .attr("d", pathGenerator);
+
+    svg.append( "g" )
+      .attr("class","pin")
+      .selectAll("path")
       .data( latlong.features)
       .enter()
       .append("path")
-      .attr("class","pin")
-      .attr( "d", path );
+      .attr( "d", pathGenerator);
+/*
+    svg.append("path")
+               .datum({type: "LineString", coordinates: [[-122.6795,45.5128],[-46.62890,-23.54640]]})
+               .attr("style","stroke: red; stroke-width: 2px; fill: none")
+               .attr("d", path);
+     svg.append("path")
+              .datum({type: "LineString", coordinates: [[-122.6795,45.5128],[151.19820,-33.86120]]})
+              .attr("style","stroke: red; stroke-width: 2px; fill: none")
+              .attr("d", path);*/
+/*
+      svg.append("path")
+               .datum({type: "LineString", coordinates: [[-122.6795,45.5128],[151.19820,-33.86120]]})
+               .attr("style","stroke: red; stroke-width: 2px; fill: none")
+               .attr("d", pathGenerator);*/
+/*
+     for(var i=0;i<retCords.length; i++){
+       var
+     }*/
+
+      var p1 = projection([-122.6795,45.5128]);
+      var p2 = projection([151.19820,-33.86120])
+      var lineData = [{"x":p1[0], "y":p1[1]},{"x":p2[0],"y":p2[1]}];
+
+      //This is the accessor function we talked about above
+      var lineFunction = d3.line()
+        .x(function(d) { return d.x; })
+        .y(function(d) { return d.y; });
+
+      svg.append("path")
+      .attr("d", lineFunction(lineData))
+      .attr("stroke", "blue")
+      .attr("stroke-width", 2)
+      .attr("fill", "none");
+      
+
 }
