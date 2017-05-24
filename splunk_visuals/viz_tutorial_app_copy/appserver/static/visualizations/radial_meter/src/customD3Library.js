62fa42df97cfec6316e372b3ac30c6aef423229f
Austin Blackman <austin.blackman@gmail.com>
2017-05-24 12:27:06 -0400

    stupid id mistake, this works for just the modal window.  continuing to build the combined modal and line

--- splunk_visuals/viz_tutorial_app_copy/appserver/static/visualizations/radial_meter/src/customD3Library.js
+++ splunk_visuals/viz_tutorial_app_copy/appserver/static/visualizations/radial_meter/src/customD3Library.js
@@ -3,6 +3,8 @@
   function customD3Library(){
     var _v = {};
 
+    var modalInit = false;
+
     _v.draw_curve = function(Ax, Ay, Bx, By, M) {
       // Find midpoint J
       var Jx = Ax + (Bx - Ax) / 2
@@ -36,6 +38,39 @@
     _v.draw_line = function(lineData){
       var ret  = "M " + lineData[0]+" "+lineData[1]+ " L "+lineData[2]+ " "+lineData[3];
       return ret;
+    }
+
+    _v.createModal = function(){
+      if(!modalInit){
+        /*
+        var iDiv = document.createElement('div');
+        iDiv.id = 'world-map-hover-modal';
+        //iDiv.className = "hiddenmodal";
+        document.getElementsByTagName('body')[0].appendChild(iDiv);
+*/
+        //new try
+        var containerDiv = document.createElement('div');
+        containerDiv.id = 'world-map-modal-container';
+
+        var lineBackDiv = document.createElement('div');
+        lineBackDiv.id = 'world-map-connect-background';
+
+        var lineDiv = document.createElement('div');
+        lineDiv.id = 'world-map-connect-line';
+
+        var modalDiv = window.document.createElement('div');
+        modalDiv.id = 'world-map-hover-modal';
+/*
+        document.getElementsByTagName('body')[0].appendChild(containerDiv);
+
+        containerDiv.appendChild(lineBackDiv);
+        containerDiv.appendChild(modalDiv);
+        lineBackDiv.appendChild(lineDiv);
+*/
+        window.document.getElementsByTagName('body')[0].appendChild(modalDiv);
+
+      }
+      modalInit = true;
     }
 
     return _v;
